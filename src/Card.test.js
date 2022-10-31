302c7b024bb3a3ca7a67f8100e44531ba16ab89f
rrghiker <chris.ryan.hart1@gmail.com>
2022-10-31 11:33:34 -0400

    step 3 complete

--- src/Card.test.js
+++ src/Card.test.js
@@ -1,3 +1,15 @@
 import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import Card from "./Card";
+
+
+it("should render Card component", function() {
+    render(<Card />);
+  });
+
+it("should match snapshot", function() {
+    const { asFragment } = render(<Card />);
+  
+    expect(asFragment()).toMatchSnapshot();
+  
+  });
--- src/Carousel.test.js
+++ src/Carousel.test.js
@@ -17,3 +17,36 @@ it("works when you click on the right arrow", function() {
   expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).not.toBeInTheDocument();
   expect(queryByAltText("Photo by Pratik Patel on Unsplash")).toBeInTheDocument();
 });
+
+
+it("works when you click on the left arrow", function() {
+  const { queryByTestId, queryByAltText } = render(<Carousel />);
+
+  // move forward in the carousel
+  const rightArrow = queryByTestId("right-arrow");
+  fireEvent.click(rightArrow);
+
+  // expect the second image to show, but not the first
+  expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).not.toBeInTheDocument();
+  expect(queryByAltText("Photo by Pratik Patel on Unsplash")).toBeInTheDocument();
+
+  const leftArrow = queryByTestId("left-arrow");
+  fireEvent.click(leftArrow);
+
+  // expect the first image to show, but not the second
+  expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).toBeInTheDocument();
+  expect(queryByAltText("Photo by Josh Post on Unsplash")).not.toBeInTheDocument();
+
+});
+
+
+it("should render Carousel component", function() {
+  render(<Carousel />);
+});
+
+it("should match snapshot", function() {
+  const { asFragment } = render(<Carousel />);
+
+  expect(asFragment()).toMatchSnapshot();
+
+});
